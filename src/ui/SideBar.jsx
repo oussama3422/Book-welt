bd08dd17a03e4f245bfebd1d2a2d010502733c76
oussama3422 <ossamarhayrhay2001@gmail.com>
2024-10-29 18:58:56 +0100

    new upds

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import MainNav from "./MainNav";
 import Logo from "./Logo";
-
-
+// import Uploader from "../data/Uploader";
 const StyledSideBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -19,6 +18,7 @@ function SideBar() {
       <Logo />
       <MainNav />
 
+      {/* <Uploader /> */}
     </StyledSideBar>
   );
 }
