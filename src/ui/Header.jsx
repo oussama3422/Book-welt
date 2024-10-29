355452cfaa22c48cfeeffcf8bf7b09936574c865
oussama3422 <ossamarhayrhay2001@gmail.com>
2024-10-29 21:23:00 +0100

    new upds

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-// import Logo from "./Logo";
-import Logout from "../features/authentication/Logout";
 import HeaderMenu from "./HeaderMenu";
 import UserAvatar from "../features/authentication/UserAvatar";
 const StyledHeader = styled.aside`
@@ -17,7 +15,6 @@ const StyledHeader = styled.aside`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
       <UserAvatar />
       <HeaderMenu />
     </StyledHeader>
